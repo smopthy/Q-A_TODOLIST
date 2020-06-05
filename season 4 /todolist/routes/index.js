1908d2a0b42f26f0b27631e842fcf81632ddaa21
smopthy <smopthy@gmail.com>
2020-06-06 01:28:07 +0800

    add login and register landing page and router

--- season 4 /todolist/routes/index.js
+++ season 4 /todolist/routes/index.js
@@ -3,8 +3,11 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const todos = require('./modules/todo')
+const users = require('./modules/users')
+
 
 router.use('/' , home)
 router.use('/todos' , todos)
+router.use('/users' , users)
 
 module.exports = router
