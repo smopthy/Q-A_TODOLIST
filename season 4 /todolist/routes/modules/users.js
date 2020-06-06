2db1cd165fa1e942ff7187e74fa88f5faa97532f
smopthy <smopthy@gmail.com>
2020-06-06 23:08:06 +0800

    logout setting

--- season 4 /todolist/routes/modules/users.js
+++ season 4 /todolist/routes/modules/users.js
@@ -52,6 +52,9 @@ router.post('/register' ,(req ,res)=>{
         })
 
 })
-
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/users/login')
+  })
 
 module.exports = router
