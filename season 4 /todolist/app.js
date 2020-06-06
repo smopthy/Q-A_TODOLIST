120679659591aa8bd143e8843709b883aa980a4e
smopthy <smopthy@gmail.com>
2020-06-07 01:44:10 +0800

    "feat: add isAuthenticated to display different view

--- season 4 /todolist/app.js
+++ season 4 /todolist/app.js
@@ -4,9 +4,10 @@ const usePassport = require('./config/passport')
 const exphbs = require('express-handlebars')
 
 const bodyParser = require('body-parser')
-const Todo = require('./model/todo')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
+const Todo = require('./model/todo')
 
 require('./config/mongoose')
 const routes = require('./routes')
@@ -26,11 +27,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
-
+app.use(flash())
 app.use((req, res, next) => {
     // 你可以在這裡 console.log(req.user) 等資訊來觀察
     res.locals.isAuthenticated = req.isAuthenticated()
     res.locals.user = req.user
+    res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+    res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
     next()
   })
 
--- season 4 /todolist/routes/modules/users.js
+++ season 4 /todolist/routes/modules/users.js
@@ -24,17 +24,31 @@ router.get('/register', (req, res) => {
 
 
 router.post('/register' ,(req ,res)=>{
-    // const email = req.body.email
-    // const name = req.body.name
-    // const password  = req.body.password
-    // const confirmPassword = req.body.confirmPassword
+
     const { name, email, password, confirmPassword } = req.body
-    
+    const errors = []
+        if (!name || !email || !password || !confirmPassword) {
+            errors.push({ message: '所有欄位都是必填。' })
+        }
+        if (password !== confirmPassword) {
+            errors.push({ message: '密碼與確認密碼不相符！' })
+        }
+        if (errors.length) {
+            return res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            confirmPassword
+            })
+        }
     User.findOne({email})    
         .then( user =>{
                 if (user) {
                     console.log('the email has been register')
-                    res.render('register',{
+                    errors.push({ message: '這個 Email 已經註冊過了。' })
+                    return res.render('register',{
+                        errors,
                         name,
                         email,
                         password,
@@ -55,6 +69,7 @@ router.post('/register' ,(req ,res)=>{
 })
 router.get('/logout', (req, res) => {
     req.logout()
+    req.flash('success_msg', '你已經成功登出。')
     res.redirect('/users/login')
   })
 
